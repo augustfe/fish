4c3fd8d3c887191bc71dcc980a58e654f78fe46f
August Femtehjell <augustfemtehjell@eduroam-193-157-169-3.wlan.uio.no>
2025-10-07 17:52:01 +0200

    make starship prompt transient

--- config.fish
+++ config.fish
@@ -7,5 +7,16 @@ fish_add_path /Applications/WezTerm.app/Contents/MacOS
 
 starship init fish | source
 
+# Make starship prompt transient
+function starship_transient_prompt_func
+    starship module character
+end
+
+function starship_transient_rprompt_func
+    starship module time
+end
+
+enable_transience
+
 abbr --add lg lazygit
 abbr --add dotdot --regex '^\.\.+$' --function multicd
